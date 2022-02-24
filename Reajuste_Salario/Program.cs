676dc09d9a4b5a4dc6e2fe92b03c4eb98c9ed1e4
Fabio2222 <fabiomoreira73@outlook.com>
2022-02-24 10:52:47 -0300

    Add project files.

---
+++ Reajuste_Salario/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Reajuste_Salario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, reajuste;
+                      
+            Console.WriteLine("Digite seu salario");
+            salario = double.Parse(Console.ReadLine());
+            if (salario < 200) 
+            {
+               Console.WriteLine("Sem reajuste");
+            }
+            else             
+            {
+                if(salario < 400)
+                {
+                    reajuste = salario * 1.20;
+                    Console.WriteLine("Seu novo salario e {0}", reajuste);
+                }
+                else
+                {
+                    if (salario < 600)
+                    {
+                        reajuste = salario * 1.40;
+                        Console.WriteLine("Seu novo salario e {0}", reajuste);
+                    }
+                    else
+                    {
+                        reajuste = salario * 1.60;
+                        Console.WriteLine("Seu novo salario e {0}", reajuste);
+                    }
+                }
+            }                           
+        }
+    }
+}
